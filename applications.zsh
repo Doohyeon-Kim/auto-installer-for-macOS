15cf071524b2a096bf85ab6aa19b1a8c765d3676
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-06-22 20:10:42 +0900

    Update applications.zsh

--- applications.zsh
+++ applications.zsh
@@ -7,4 +7,8 @@ brew install --cask google-chrome
 # Insatall ITerm2
 brew install --cask iterm2
 
+# Install spectacle
+brew install --cask spectacle
 
+# Install appcleaner
+brew install --cask appcleaner
