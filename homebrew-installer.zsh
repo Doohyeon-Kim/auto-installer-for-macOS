04663924408deec0529f193582e8ab5b56a2dae5
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-06-22 21:22:44 +0900

    Update homebrew-installer.zsh

--- homebrew-installer.zsh
+++ homebrew-installer.zsh
@@ -5,3 +5,5 @@ mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar 
 eval $(/opt/homebrew/bin/brew shellenv)
 
 echo "HOMEBREW_PATH=/opt/homebrew/bin" >> $HOME/.zshrc
+
+echo "eval $(/opt/homebrew/bin/brew shellenv)" >> $HOME/.zshrc
