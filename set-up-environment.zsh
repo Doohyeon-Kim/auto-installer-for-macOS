61ec11655204938f022c164e192de102f5625cce
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-06-22 19:37:30 +0900

    Create set-up-environment.zsh

---
+++ set-up-environment.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+     
