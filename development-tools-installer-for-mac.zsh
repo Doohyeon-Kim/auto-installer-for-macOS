2c8dd1e0b3385bc703810eb056e915b49539579e
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-06-22 19:40:04 +0900

    Update development-tools-installer-for-mac.zsh

--- development-tools-installer-for-mac.zsh
+++ development-tools-installer-for-mac.zsh
@@ -3,16 +3,21 @@
 chmod +x *.zsh
 
 echo "" >> $HOME/.zshrc
-./homebrew-installer.zsh
+./essential.zsh
 source $HOME/.zshrc
-./android-studio-installer.zsh
-./java11-installer.zsh
+./xcode-setup.zsh
+./git.zsh
+./intellij-ultimate.zsh
 source $HOME/.zshrc
 ./flutter-installer.zsh
+./java11-installer.zsh
 ./nestjs-installer.zsh
 ./postgresql-installer.zsh
+./android-studio-installer.zsh
 ./terraform-installer.zsh
 ./ansible-installer.zsh
 ./packer-installer.zsh
+source $HOME/.zshrc
+./set-up-environment.zsh
 
 sudo rm -rf $HOME/development-tools-installer-for-mac
