3fd46addc20abe8371e4a624c91146367b44829c
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-10-15 12:12:32 +0900

    Update auto-installer-for-macOS.zsh

--- auto-installer-for-macOS.zsh
+++ auto-installer-for-macOS.zsh
@@ -14,14 +14,14 @@ source $HOME/.zshrc
 ./java11-installer.zsh
 ./nestjs-installer.zsh
 ./postgresql-installer.zsh
-./android-studio-installer.zsh
+##./android-studio-installer.zsh
 ./terraform-installer.zsh
-./ansible-installer.zsh
-./packer-installer.zsh
+##./ansible-installer.zsh
+##./packer-installer.zsh
 source $HOME/.zshrc
 ./set-up-environment.zsh
 
 
-echo "export PATH=\"\$PATH:\$HOMEBREW_PATH\$FLUTTER_PATH\"" >> $HOME/.zshrc
+echo "export PATH=\"\$PATH:\$FLUTTER_PATH\"" >> $HOME/.zshrc
 
 sudo rm -rf $HOME/auto-installer-for-macOS
